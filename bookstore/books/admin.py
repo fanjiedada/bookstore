837fd1b97893e778b0bcc06442aadbe3e78e0615
fanjiedada <1263268606@qq.com>
2018-06-08 15:25:24 +0800

    add book

---
+++ bookstore/books/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from django.contrib import admin
+from books.models import Books
+
+
+# Register your models here.
+
+admin.site.register(Books) #在admin中添加有关商品的编辑功能
+
+
